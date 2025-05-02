18afcc6fe5adbfc85c2672336f5158a2966ef32a
DarkFerrari <171889029+DarkFerrari@users.noreply.github.com>
2025-05-02 14:57:11 -0500

    add square input dynamically
    add prompt to enter no of squares to fill the screen

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -12,16 +12,31 @@ container.style.padding = "0";
 container.style.boxSizing="border-box"
 
 console.log(visualViewport)
-
-const boxSize=60;
-const columns = Math.floor(window.innerWidth/boxSize)
-console.log(columns)
-const rows = Math.floor(window.innerHeight/boxSize)
-console.log(rows)
-const totalBoxes = (columns * rows)
+totalBoxes=25
 
 
-for(i=0; i<totalBoxes; i++)
+
+const button = document.querySelector(".button")
+let squareInput = ""
+button.addEventListener("click", ()=> {
+    do{
+        squareInput = prompt("enter the number of squares less than 100")
+    } while (isNaN(squareInput) || squareInput>100)
+    
+    console.log(squareInput)
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+    displayBoxes(squareInput)
+    })
+
+
+function displayBoxes(givenBoxes)
+    {
+const columns = Math.ceil(Math.sqrt(givenBoxes))
+const boxSize= 100 /columns;
+
+for(i=0; i<givenBoxes; i++)
 {
 const div = document.createElement("div")
 div.classList.add("divs")
@@ -33,9 +48,8 @@ div.style.boxSizing="border-box"
 div.style.justifyContent="center"
 div.style.alignItems="center"
 div.style.backgroundColor="red"
-div.style.width=`${boxSize}px`
-div.style.height=`${boxSize}px`
-div.style.backgrou
+div.style.width=`${boxSize}vw`
+div.style.height=`${boxSize}vh`
 div.textContent="testing"
 div.addEventListener("mouseover",()=> 
     {
@@ -50,4 +64,8 @@ div.addEventListener("mouseover",()=>
     })
 container.appendChild(div)
 }
+    }
+
+displayBoxes(totalBoxes)
+
     
